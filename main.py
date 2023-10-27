36d4bd249a0a088c8d00b082d789650aa8c44ce6
Medha Tripathi <tmedha01@gmail.com>
2023-10-27 15:53:21 -0500

    Add some Python related Python files

---
+++ main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Manobal actually sucks ngl.")
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,5 @@
+def test():
+    print("Testing 1 2 3.")
+
+if __name__ == '__main__':
+    test()

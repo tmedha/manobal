57d4d61050b119b291230677011f71340cc05173
Medha Tripathi <tmedha01@gmail.com>
2023-10-31 10:42:38 -0500

    Remove save_creds() calling

--- body.py
+++ body.py
@@ -4,10 +4,12 @@ creds = {}
 
 def add_cred(platform, username, password):
     creds[platform] = {"username": username, "password": password}
+    save_creds()
 
 def remove_cred(platform) :
     del creds[platform]
     # creds.pop(platform)
+    save_creds()
 
 def is_cred_present(platform):
     return platform in creds
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from body import creds, add_cred, remove_cred, is_cred_present, save_creds
+from body import creds, add_cred, remove_cred, is_cred_present
 
 def test():
     print("Testing 1 2 3.")
@@ -7,7 +7,6 @@ def test():
     # add_cred('Microsoft', 'Medha', 'password')
     remove_cred('windows')
     print(is_cred_present('windows'))
-    save_creds()
 
 if __name__ == '__main__':
     test()

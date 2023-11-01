b0c19346d47a08568ab0fc01f47ca4fdb4dca1dd
Medha Tripathi <tmedha01@gmail.com>
2023-11-01 09:53:40 -0500

    Update print in test.py

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 from body import creds, add_cred, remove_cred, is_cred_present, load_creds
+from pprint import pp
 
 def test():
     print("Testing 1 2 3.")
     load_creds()
-    print(creds)
-    # add_cred('windows', 'paramsiddharth', 'lallupassword123')
-    # add_cred('Microsoft', 'Medha', 'password')
+    add_cred('windows', 'paramsiddharth', 'lallupassword123')
+    add_cred('Microsoft', 'Medha', 'password')
     # remove_cred('windows')
+    # remove_cred('Microsoft')
     # print(is_cred_present('windows'))
+    pp(creds)
 
 if __name__ == '__main__':
     test()

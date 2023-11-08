437fe111eb1cc613590a212af4dde700905d17b8
Medha Tripathi <tmedha01@gmail.com>
2023-11-08 00:29:10 -0600

    Loop the code

--- main.py
+++ main.py
@@ -4,48 +4,49 @@ def main():
     #Likho welcome to manobal
     print('Hello everyone!\nWelcome to Manobal, your neighbourhood password manager which locally encrypts your credentials.')
     #List dikhao platforms ka
-    load_creds()
-    display_platforms()
-    #Karna kya hai user (Add/Edit/Update, View, Delete)
-    print('Choose what operation you would like to perform:')
-    print('Press:\n1. For Adding/Updating a Credential.')
-    print('2. For Deleting an Existing Credential.')
-    print('3. For Viewing Existing Credentials.')
-    print('4. Exit.\n')
-    #User se number input karna hai
-    choice = input("Enter your choice: ")
+    while True:
+        load_creds()
+        display_platforms()
+        #Karna kya hai user (Add/Edit/Update, View, Delete)
+        print('Choose what operation you would like to perform:')
+        print('Press:\n1. For Adding/Updating a Credential.')
+        print('2. For Deleting an Existing Credential.')
+        print('3. For Viewing Existing Credentials.')
+        print('4. Exit.\n')
+        #User se number input karna hai
+        choice = input("Enter your choice: ")
 
-    if choice == '1':
-        platform = input("Enter the platform for which you have credentials: ")
-        username = input("Enter your username: ")
-        password = input("Enter your password: ")
-        add_cred(platform, username, password)
-        # save_creds()
-
-    elif choice == '2':
-        platform_delete = input('Enter platform that you want to delete: ')
-        if is_cred_present(platform_delete):
-            remove_cred(platform_delete)
-            print('Platform and linked credentials deleted.')
+        if choice == '1':
+            platform = input("Enter the platform for which you have credentials: ")
+            username = input("Enter your username: ")
+            password = input("Enter your password: ")
+            add_cred(platform, username, password)
             # save_creds()
+
+        elif choice == '2':
+            platform_delete = input('Enter platform that you want to delete: ')
+            if is_cred_present(platform_delete):
+                remove_cred(platform_delete)
+                print('Platform and linked credentials deleted.')
+                # save_creds()
+            else:
+                print('Credential not found.')
+
+        elif choice == '3':
+            view_choice = input('Enter platform to view (enter A/All for viewing all): ')
+            if is_cred_present(view_choice):
+                view_credentials(view_choice)
+            elif view_choice == 'All' or 'all' or 'A' or 'a':
+                view_all_credentials(view_choice)
+            else:
+                print('Invalid credential.')
+
+
+        elif choice == '4':
+            ...
+
         else:
-            print('Credential not found.')
-
-    elif choice == '3':
-        view_choice = input('Enter platform to view (enter A/All for viewing all): ')
-        if is_cred_present(view_choice):
-            view_credentials(view_choice)
-        elif view_choice == 'All' or 'all' or 'A' or 'a':
-            view_all_credentials(view_choice)
-        else:
-            print('Invalid credential.')
-
-
-    elif choice == '4':
-        ...
-
-    else:
-        print('Invalid choice.')
+            print('Invalid choice.')
 
 if __name__ == '__main__':
     main()
